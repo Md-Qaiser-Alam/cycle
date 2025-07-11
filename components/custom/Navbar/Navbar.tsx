26c2cc0f1a7d081acbbf300a6175f3a5321c529e
Md-Qaiser-Alam <mdq687@gmail.com>
2025-07-12 04:14:40 +0530

    add ham menu

--- components/custom/Navbar/Navbar.tsx
+++ components/custom/Navbar/Navbar.tsx
@@ -9,8 +9,17 @@ import {
   NavigationMenuList,
   NavigationMenuTrigger,
 } from "@/components/ui/navigation-menu";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 import { Fade as Hamburger } from "hamburger-react";
 import Link from "next/link";
+import { useState } from "react";
 
 const navLinksData = [
   {
@@ -47,6 +56,7 @@ const navLinksData = [
 ];
 
 function Navbar() {
+  const [isMenuVisible, setIsMenuVisible] = useState(false);
   return (
     <nav className="fixed top-0 inset-x-0 z-[99] px-4 py-3 bg-[rgba(255, 255, 255, 0.8)] backdrop-blur-md">
       <div className="flex items-center justify-between max-w-[var(--breathing-width-1)] mx-auto px-4 md:px-8">
@@ -110,8 +120,36 @@ function Navbar() {
 
         {/* Mobile Hamburger */}
         <div className="md:hidden cursor-pointer">
-          <Hamburger direction="right" size={20} />
+          <Hamburger
+            toggled={isMenuVisible}
+            toggle={setIsMenuVisible}
+            direction="right"
+            size={20}
+          />
         </div>
+
+        {isMenuVisible && (
+          <Sheet open={isMenuVisible} onOpenChange={setIsMenuVisible}>
+            <SheetContent side="top" className="z-[99] py-5 rounded-2xl m-2">
+              <SheetHeader className="flex flex-col justify-center items-center space-y-3">
+                {navLinksData.map((e, i) => (
+                  <SheetTitle key={i}>
+                    <a href="#">{e.name}</a>
+                  </SheetTitle>
+                ))}
+              </SheetHeader>
+
+              <div className="flex flex-col justify-center items-center">
+                <Button className="text-black py-6 hover:bg-gray-100 bg-transparent border-none shadow-none text-md">
+                  Log in
+                </Button>
+                <Button size={"lg"} className="rounded-xl py-6 text-md w-40">
+                  Get Started
+                </Button>
+              </div>
+            </SheetContent>
+          </Sheet>
+        )}
       </div>
     </nav>
   );
